3e22b22acf70ea105412c4afa5ca0713253f40d3
stephane71183101 <stephane71183101@gmail.com>
2022-06-27 16:08:40 +0200

    Add article controller, model, migration and seeder
    3 articles created, string with mediumtext for content column replaced, article category inserted

---
+++ database/seeders/ArticlesSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ArticlesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('articles')->insert([
+            "category" => "industry",
+            "title" => "Article 1",
+            "short" => "This is short of article 1.",
+            "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "user_id" => 1,
+            "filename_image" => "image1.png"
+        ]);
+
+        DB::table('articles')->insert([
+            "category" => "medical",
+            "title" => "Article 2",
+            "short" => "This is short of article 2.",
+            "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "user_id" => 1,
+            "filename_image" => "image2.png"
+        ]);
+
+        DB::table('articles')->insert([
+            "category" => "robotics",
+            "title" => "Article 3",
+            "short" => "This is short of article 3.",
+            "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "user_id" => 2,
+            "filename_image" => "image3.png"
+        ]);
+    }
+}
