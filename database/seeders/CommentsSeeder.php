6e25aa491af3aa8f16eeb603f05cbb013a06f7e8
Rico Pfister <contact.rico.pfister@gmail.com>
2022-06-29 10:00:27 +0200

    Changed CommentsController
    added column article_id
    changed 2022_06_27_124842_create_comments_table.php to 2022_06_26_124842_create_comments_table.php
    
    cleaned up database/seeders/CommentsSeeder.php and added 3 comments

--- database/seeders/CommentsSeeder.php
+++ database/seeders/CommentsSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class CommentsSeeder extends Seeder
 {
@@ -16,11 +17,27 @@ class CommentsSeeder extends Seeder
     public function run() {
 
         DB::table('comments')->insert([
-            // 'id' => '1',
             'user_id' => '1',
-            'text' => 'test comment',
-            // 'created_at' => '',
-            // 'updated_at' => '',
+            'article_id' => '1',
+            'text' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.',
+            'created_at' => '2022-06-27 07:51:03',
+            'updated_at' => '2022-06-27 07:51:03'
+        ]);
+
+        DB::table('comments')->insert([
+            'user_id' => '2',
+            'article_id' => '2',
+            'text' => 'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.',
+            'created_at' => '2022-06-28 05:51:03',
+            'updated_at' => '2022-06-28 05:51:03'
+        ]);
+
+        DB::table('comments')->insert([
+            'user_id' => '3',
+            'article_id' => '3',
+            'text' => 'no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+            'created_at' => '2022-06-29 12:44:03',
+            'updated_at' => '2022-06-29 12:44:03'
         ]);
     }
 }
