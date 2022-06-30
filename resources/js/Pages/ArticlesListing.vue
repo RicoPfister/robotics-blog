228c5b3f6a58e45c46fd89a032933a3c06354ff3
IngoForrerNuila <ingoproductions@gmail.com>
2022-06-30 09:34:23 +0200

    article-listing-completed

--- resources/js/Pages/ArticlesListing.vue
+++ resources/js/Pages/ArticlesListing.vue
@@ -1,9 +1,25 @@
 <template>
     <MainLayout>
-        <div class="text-5xl">Article Listing</div>
+        <div class="container mx-auto columns-1">
+            <div class="text-black text-2xl text-center mt-10 mb-10">
+                CATEGORY/SECTION NAME
+            </div>
+            <ArticleBrief image-left="true"></ArticleBrief>
+            <ArticleBrief></ArticleBrief>
+            <ArticleBrief image-left="true"></ArticleBrief>
+            <ArticleBrief></ArticleBrief>
+            <ArticleBrief image-left="true"></ArticleBrief>
+            <ArticleBrief></ArticleBrief>
+            <ArticleBrief image-left="true"></ArticleBrief>
+            <ArticleBrief></ArticleBrief>
+        </div>
+
     </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from "@/Layouts/MainLayout.vue";
+import MyImage from "@/../images/botblog-logo-2.png";
+import MyLike from "@/../images/like.png";
+import ArticleBrief from "../Components/ArticleBrief.vue";
 </script>
