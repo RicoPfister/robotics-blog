d9487ea39799a5859a4e1b6da22da289ccc20771
JuergenSchneider25 <juergenschneider2512@gmail.com>
2022-06-30 12:17:27 +0200

    Input Title Input Description 2 neue Boxen

--- resources/js/Pages/NewArticle.vue
+++ resources/js/Pages/NewArticle.vue
@@ -6,10 +6,19 @@
 
 <div class="container mx-auto columns-2">
 
+<form>
+  <label for="Title">Title:</label><br>
+  <input type="text" id="Title" name="Title"><br>
+  <label for="Description">Description:</label><br>
+  <input type="text" id="Description" name="Description">
+</form>
+
     <div class="child-text-cont">
      </div><div class="mx-auto text-center">
      <textarea class="shadow rounded stroke-[2px] " rows="15" cols="39" name="...." placeholder="..."> </textarea>
     </div></div>
+
+
 
 <div class="inline-flex rounded-md shadow-sm" role="group">
   <button type="button" class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
@@ -29,7 +38,7 @@
 
 <div class="inline-flex rounded-md shadow-sm" role="group">
   <button type="button" class="py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-l-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-    Add Img
+    Upload File
   </button>
   <button type="button" class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
     Remove
