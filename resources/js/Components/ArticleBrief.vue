4f695060d8eec06e23f151e4929a38ac23c5a603
IngoForrerNuila <ingoproductions@gmail.com>
2022-07-06 09:20:04 +0200

    Article Listing Upgrade Here I have change how the article structure was designed and added a media query to enhance the mobile perspective

--- resources/js/Components/ArticleBrief.vue
+++ resources/js/Components/ArticleBrief.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex">
-        <div v-if="imageLeft" class="flex mx-auto">
-            <div class="flex flex-col item-center font-bold text-purple-500 text-3xl text-center">
+        <div v-if="imageLeft" class="flex items-center mx-auto ingoMOD">
+            <div class=" font-bold text-purple-500 text-3xl text-center">
                 ARTICLE TITLE
-                <img :src="MyImage" class="h-70 w-100"/>
+                <img :src="MyGirlRobot" class="scale-75 rounded-full"/>
             </div>
-            <div class="">
+            <div class="py-4">
                 This is a little description about the article, this might be
                 written in a excel table with all the other details together.
                 What are you waiting for? Do not lose the chance to be part of
@@ -16,8 +16,8 @@
                 Sign Up Now!!!
             </div>
         </div>
-        <div v-else class="flex mx-auto">
-            <div class="">
+        <div v-else class="flex items-center mx-auto ingoMOD">
+            <div class="py-4">
                 This is a little description about the article, this might be
                 written in a excel table with all the other details together.
                 What are you waiting for? Do not lose the chance to be part of
@@ -30,14 +30,18 @@
 
             <div class="flex flex-col item-center font-bold text-purple-500 text-3xl text-center">
                 ARTICLE TITLE
-                <img :src="MyImage" class="h-70 w-100" />
+                <img :src="MyGirlRobot" class="scale-75 rounded-full" />
             </div>
         </div>
     </div>
 </template>
 
+
+</style>
+
 <script setup>
 import MyImage from "@/../images/botblog-logo-2.png";
+import MyGirlRobot from "@/../images/robot-girl-image.jpg";
 
 defineProps({
     imageLeft: Boolean,
--- resources/js/Pages/ArticlesListing.vue
+++ resources/js/Pages/ArticlesListing.vue
@@ -1,25 +1,96 @@
 <template>
     <MainLayout>
-        <div class="container mx-auto columns-1">
-            <div class="text-black text-2xl text-center mt-10 mb-10">
+        <!--HERE GOES THE NAME OF THE CATEGORY SELECTED BY THE USER-->
+        <div class="container mx-auto ">
+            <div class="container mx-auto text-center font-bold text-black text-3xl p-4">
                 CATEGORY/SECTION NAME
             </div>
-            <ArticleBrief image-left="true"></ArticleBrief>
-            <ArticleBrief></ArticleBrief>
-            <ArticleBrief image-left="true"></ArticleBrief>
-            <ArticleBrief></ArticleBrief>
-            <ArticleBrief image-left="true"></ArticleBrief>
-            <ArticleBrief></ArticleBrief>
-            <ArticleBrief image-left="true"></ArticleBrief>
-            <ArticleBrief></ArticleBrief>
+
+            <div class="flex items-center mx-auto ingoMOD">
+                <!--HERE GOES THE NAME/TITLE OF THE ARTICLE REGARDING THE CATEGORY SELECTED BY USER-->
+                <div class=" font-bold text-purple-500 text-3xl text-center">
+                     ARTICLE TITLE
+                    <!--HERE GOES THE GOES THE IMAGE RELATED WITH THE ARTICLE-->
+                    <img :src="MyGirlRobot" class="scale-75 rounded-full"/>
+                </div>
+                <!--HERE GOES A LITTLE DESCRIPTION OF THE ARTICLE-->
+                <div class="py-4">
+                     This is a little description about the article, this might be
+                     written in a excel table with all the other details together.
+                     What are you waiting for? Do not lose the chance to be part of
+                     something special and Sign Up Now!!! This is a little
+                     description about the article, this might be written in a excel
+                     with all the other details together. What are you waiting
+                     for? Do not lose the chance to be part of something special and
+                     Sign Up Now!!!
+                </div>
+            </div>
+
+            <div class="flex items-center mx-auto ingoMOD">
+                <!--HERE GOES THE NAME/TITLE OF THE ARTICLE REGARDING THE CATEGORY SELECTED BY USER-->
+                <div class=" font-bold text-purple-500 text-3xl text-center">
+                    ARTICLE TITLE
+                    <!--HERE GOES THE GOES THE IMAGE RELATED WITH THE ARTICLE-->
+                    <img :src="MyGirlRobot" class="scale-75 rounded-full"/>
+                </div>
+                <!--HERE GOES A LITTLE DESCRIPTION OF THE ARTICLE-->
+                <div class="py-4">
+                     This is a little description about the article, this might be
+                     written in a excel table with all the other details together.
+                     What are you waiting for? Do not lose the chance to be part of
+                     something special and Sign Up Now!!! This is a little
+                     description about the article, this might be written in a excel
+                     with all the other details together. What are you waiting
+                     for? Do not lose the chance to be part of something special and
+                     Sign Up Now!!!
+                </div>
+            </div>
+
+            <div class="flex items-center mx-auto ingoMOD">
+                <!--HERE GOES THE NAME/TITLE OF THE ARTICLE REGARDING THE CATEGORY SELECTED BY USER-->
+                <div class=" font-bold text-purple-500 text-3xl text-center">
+                    ARTICLE TITLE
+                    <!--HERE GOES THE GOES THE IMAGE RELATED WITH THE ARTICLE-->
+                    <img :src="MyGirlRobot" class="scale-75 rounded-full"/>
+                </div>
+                <!--HERE GOES A LITTLE DESCRIPTION OF THE ARTICLE-->
+                <div class="py-4">
+                     This is a little description about the article, this might be
+                     written in a excel table with all the other details together.
+                     What are you waiting for? Do not lose the chance to be part of
+                     something special and Sign Up Now!!! This is a little
+                     description about the article, this might be written in a excel
+                     with all the other details together. What are you waiting
+                     for? Do not lose the chance to be part of something special and
+                     Sign Up Now!!!
+                </div>
+            </div>
+
         </div>
 
     </MainLayout>
 </template>
 
+<!--HERE GOES A MEDIA QUERY THAT MAKES THE WHOLE SITE CHANGING THE DIV STRUCTURE AT CERTAIN PIXEL OR SCREEN SIZE-->
+<style>
+    @media (max-width: 761px) {
+    .ingoMOD {
+    display: flex;
+    flex-direction: column;
+    }
+
+    .py-4 {
+    margin: 1rem;
+
+}
+}
+</style>
+
+<!--HERE WE HAVE THE IMAGES IMPORTED IN ORDER TO BE USED IN THIS SECTION-->
 <script setup>
 import MainLayout from "@/Layouts/MainLayout.vue";
 import MyImage from "@/../images/botblog-logo-2.png";
 import MyLike from "@/../images/like.png";
 import ArticleBrief from "../Components/ArticleBrief.vue";
+import MyGirlRobot from "@/../images/robot-girl-image.jpg";
 </script>
