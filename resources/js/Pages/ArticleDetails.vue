6f84b1695befe65d9ef3fc32659187caff35dfaa
Rico Pfister <contact.rico.pfister@gmail.com>
2022-07-07 12:31:16 +0200

    change Welcome; ArticleDetails; ArticlesController; route web
    Show 3 random articles on the starting page
    Show comments related to the article

--- resources/js/Pages/ArticleDetails.vue
+++ resources/js/Pages/ArticleDetails.vue
@@ -1,62 +1,52 @@
 <template>
     <MainLayout>
-
         <div
-            class="container mx-auto text-center font-bold text-white text-3xl p-2 bg-slate-500"
-        >
+            class="container mx-auto text-center font-bold text-white text-3xl p-2 bg-slate-500">
             <p>{{ article.title }}</p>
         </div>
 
+        <!-- HERE IS WHERE THE ARTICLES IMAGE IS CALLED -->
         <div class="container mx-auto flex justify-center">
-
-            <img
-                v-if="article.filename_image != null"
-                :src="'/images/' + article.filename_image"
-            />
-            <img v-else src="/images/default.webp" />
+            <img :src="'/images/' + article.filename_image" />
         </div>
 
         <div
-            class="container mx-auto first-letter:text-7xl first-line:uppercase first-letter:font-bold text-left text-white bg-slate-500"
-        >
+            class="container mx-auto first-letter:text-7xl first-line:uppercase first-letter:font-bold text-left text-white bg-slate-500">
             <div>
                 {{ article.content }}
-
-                <div
-                    class="mx-auto text-center font-bold text-white text-1xl bg-sky-400"
-                >
-                    {{ article.created_at_human }} | Created by
-                    {{ article.author }}
+                <!-- HERE IS WERE THE ARTICLES DATE AND WRITER IS CALLED OR WRITTEN  -->
+                <div class="mx-auto text-center font-bold text-white text-1xl bg-sky-400">
+                    {{ article.created_at_human }} | Created by {{ article.author }}
                 </div>
+
             </div>
+
         </div>
+        <!-- HERE IS WERE PEOPLE CAN LIKE OR DISLIKE THE ARTICLE -->
+        <div class="columns-2 text-center content-start rounded  mx-auto p-3 m-5 bg-sky-400" >
 
         <div
-            class="columns-2 text-center content-start rounded mx-auto p-3 m-5 bg-sky-400"
-        >
+            class="columns-2 text-center content-start rounded mx-auto p-3 m-5 bg-sky-400">
             <div class="font-bold text-center text-white">
                 169
                 <button
-                    class="text-center font-bold bg-sky-700 rounded mx-auto p-2 g-sky-600 hover:bg-sky-900 text-white"
-                >
+                    class="text-center font-bold bg-sky-700 rounded mx-auto p-2 g-sky-600 hover:bg-sky-900 text-white">
                     &#128077;
+
                 </button>
             </div>
 
-            <div class="font-bold text-center text-white">
+            <div class="font-bold text-center text-white ">
+
                 68
                 <button
-                    class="text-center font-bold bg-sky-700 rounded mx-auto p-2 g-sky-600 hover:bg-sky-900 text-white"
-                >
+                    class="text-center font-bold bg-sky-700 rounded mx-auto p-2 g-sky-600 hover:bg-sky-900 text-white">
                     &#128078;
+
                 </button>
+
             </div>
         </div>
-
-        <div v-if="$page.props.user">
-            <div class="mx-auto text-center font-bold p-2">
-                <p>LEAVE A COMMENT</p>
-            </div>
 
 
             <div class="mx-auto text-center">
@@ -66,8 +56,7 @@
                     rows="7"
                     cols="40"
                     name="content"
-                    placeholder="write here"
-                >
+                    placeholder="write here">
                 </textarea>
 
                 <div class="text-red-600" v-if="$page.props.errors.content">
@@ -75,54 +64,34 @@
                 </div>
             </div>
 
-            <button
-                @click="
-                    createCommentForm.post(
-                        `/articles/${article.id}/comments`,
-                        {}
-                    )
-                "
-                class="text-center font-bold bg-teal-600 rounded mx-auto p-2 g-sky-600 hover:bg-sky-700 text-white"
-            >
-                SUBMIT
-            </button>
+                <button
+                    @click="createCommentForm.post(`/articles/${article.id}/comments`,{})"
+                    class="flex justify-center text-center font-bold bg-teal-600 rounded mx-auto p-2 g-sky-600 hover:bg-sky-700 text-white">
+                    SUBMIT
+                </button>
         </div>
 
-        <div class="mx-auto text-center font-bold m-5">
-            <p>COMMENTS</p>
+        <div v-if="commentUser.length > 0">
+
+            <div class="mx-auto text-center font-bold m-5">
+                <p>COMMENTS</p>
+            </div>
+            <div class="container p-2 rounded mx-auto display">
+
+                    <div v-for="commentDetail in commentUser" class="grid grid-cols-12 gap-4 p-2 rounded drop-shadow-xl m-3 bg-sky-400 text-white">
+                        <div class="flex flex-col col-span-2">
+                            <div class="font-bold">{{ commentDetail.name }}</div>
+                            <div class="text-xs">{{ formatDate(commentDetail.created_at) }}</div>
+                        </div>
+
+                        <div class="col-span-10">
+                        {{ commentDetail.text }}
+                        </div>
+                    </div>
+            </div>
+
         </div>
-
-        <div class="container text-center p-2 rounded mx-auto display">
-            <div class="text-center">
-                <div
-                    class="text-center p-2 rounded drop-shadow-xl m-3 bg-sky-400 text-white inline-flex"
-                >
-                    I think this is absolutely amazing I did not know that my
-                    cat could be inside the Matrix as a Djavu. But yes, believe
-                    me, If I were Neo I would not leave alone Trinity. I think
-                    this is absolutely amazing I did not know that my cat could
-                    be inside the Matrix as a Djavu. But yes, believe me, If I
-                    were Neo I would not leave alone Trinity.
-                </div>
-            </div>
-
-            <div class="text-center">
-                <div
-                    class="text-center p-2 rounded drop-shadow-xl m-3 bg-sky-400 text-white inline-flex"
-                >
-                    Vamonos!!! I do really want my cyber cat also, but, is it
-                    possible to accquire one in United Stares?
-                </div>
-            </div>
-
-            <div class="text-center">
-                <div
-                    class="text-center p-2 rounded drop-shadow-xl m-3 bg-sky-400 text-white inline-flex"
-                >
-                    This is something I need to discuse with my parrot :/
-                </div>
-            </div>
-        </div>
+         <div class="flex justify-center mb-5" v-else>No Comments found. Be the first to comment this article!</div>
     </MainLayout>
 </template>
 
@@ -131,12 +100,19 @@ import MainLayout from "@/Layouts/MainLayout.vue";
 import MyImage from "@/../images/botblog-logo-2.png";
 import MyLike from "@/../images/like.png";
 import MyGirlRobot from "@/../images/robot-girl-image.jpg";
-
 import { useForm, usePage } from "@inertiajs/inertia-vue3";
 import { computed } from "@vue/reactivity";
 import { ref } from "vue";
-
 const comment = ref("");
 const createCommentForm = useForm({ content: comment });
+const commentUser = computed(() => usePage().props.value.commentUser);
 const article = computed(() => usePage().props.value.article);
+
+function formatDate(dateString) {
+
+    const milliseconds = Date.parse(dateString)
+    const dateObject = new Date(milliseconds)
+    const humanDateFormat = dateObject.toLocaleString('de-CH', {timeStyle: 'short', dateStyle: 'short' })
+    return humanDateFormat
+}
 </script>
